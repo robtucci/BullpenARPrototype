7d6645036ea2146eb5d20715beb3bf35a2530405
Robert Bertucci <robtucci7@gmail.com>
2017-05-17 23:09:57 -0500

    made some minor adjustments to mouseclick, added min/max target size. sharing works!

--- Assets/Scripts/MouseClick.cs
+++ Assets/Scripts/MouseClick.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class MouseClick : MonoBehaviour {
 
-    public float scaleSpeed = 5;
-    private float mouseSize = 2.5f;
+    public static float SCALE_SPEED = 5f;
+    private static float STARTING_CURSOR_SIZE = 2f;
    
     private Ray ray;
     private RaycastHit hit;
@@ -35,28 +35,22 @@ public class MouseClick : MonoBehaviour {
 
     private void MoveCursor()
     {
-        //Debug.Log("mouseclick");
+
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit))
         {
-            //Debug.Log(hit.collider.name);
             if (hit.collider.name == "StrikeZone")   //if the object we hit is the strikeZone Object,
             {
                 if (this.transform.localScale.x == 0)
                 {
-                    this.transform.localScale = new Vector3(2, 2, 2);
+                    this.transform.localScale = new Vector3(STARTING_CURSOR_SIZE,STARTING_CURSOR_SIZE,STARTING_CURSOR_SIZE);
                 }
                 this.transform.position = hit.point;
                 this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
                 CustomMessages2.Instance.SendTargetData(this.transform.position, this.transform.localScale);
             
             }
-                //Debug.Log(hit.textureCoord.ToString());   //seems to be a dead end
-
-
-                //Debug.Log("World Coord: (" + hit.point.x + "," + hit.point.y + "," + hit.point.z + ")");
-                //Debug.Log("Local Coord: (" + transform.localPosition.x + ", " + transform.localPosition.y);
         }
 
         
@@ -64,13 +58,18 @@ public class MouseClick : MonoBehaviour {
 
     private void ScaleCursor()
     {
-        //Debug.Log("ScaleCursor");
-        float mouseChange = Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * scaleSpeed;
+        float MIN_CURSOR_SIZE = 1.0f; 
+        float MAX_CURSOR_SIZE = 8.0f;
+        float cursorSize = this.transform.localScale.x;
+
+        float cursorChange = Input.GetAxis("Mouse ScrollWheel") * SCALE_SPEED;
 
 
-        mouseSize += mouseChange;
-        //Debug.Log(mouseSize);
-        this.transform.localScale = new Vector3(mouseSize, mouseSize, mouseSize);
+        cursorSize += cursorChange;
+        cursorSize = Mathf.Min(MAX_CURSOR_SIZE, cursorSize);
+        cursorSize = Mathf.Max(MIN_CURSOR_SIZE, cursorSize);
+
+        this.transform.localScale = new Vector3(cursorSize,cursorSize,cursorSize);
         CustomMessages2.Instance.SendTargetData(this.transform.position, this.transform.localScale);
     }
 
