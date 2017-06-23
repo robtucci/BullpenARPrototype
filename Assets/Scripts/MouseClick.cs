d3de06a21a39d8eadf4683ea279db6a72d0c86bf
brian kaufman <briankauf@gmail.com>
2017-06-23 00:04:00 -0500

    Update UI with opacity and color sliders.
    Want to come back and make grid texture options and proper color picer

--- Assets/Scripts/MouseClick.cs
+++ Assets/Scripts/MouseClick.cs
@@ -4,73 +4,73 @@ using UnityEngine;
 
 public class MouseClick : MonoBehaviour {
 
-    public static float SCALE_SPEED = 5f;
-    private static float STARTING_CURSOR_SIZE = 2f;
+	public static float SCALE_SPEED = 5f;
+	private static float STARTING_CURSOR_SIZE = 2f;
    
-    private Ray ray;
-    private RaycastHit hit;
+	private Ray ray;
+	private RaycastHit hit;
 
 
-    
+	
 
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
 
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            MoveCursor();   //move the cursor based on user click
-        }
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			MoveCursor();   //move the cursor based on user click
+		}
 
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f || Input.GetAxis("Mouse ScrollWheel") < 0f)
-        {
-            ScaleCursor();   //scale cursor based on use of the scrollwheel
-        }
+		if (Input.GetAxis("Mouse ScrollWheel") > 0f || Input.GetAxis("Mouse ScrollWheel") < 0f)
+		{
+			ScaleCursor();   //scale cursor based on use of the scrollwheel
+		}
 
-    }
+	}
 
-    private void MoveCursor()
-    {
+	private void MoveCursor()
+	{
 
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit))
-        {
-            if (hit.collider.name == "StrikeZone")   //if the object we hit is the strikeZone Object,
-            {
-                if (this.transform.localScale.x == 0)
-                {
-                    this.transform.localScale = new Vector3(STARTING_CURSOR_SIZE,STARTING_CURSOR_SIZE,STARTING_CURSOR_SIZE);
-                }
-                this.transform.position = hit.point;
-                this.transform.rotation = Quaternion.Euler(-90, 180, 180);
-                CustomMessages2.Instance.SendTargetData(1.0f,this.transform.position, this.transform.localScale);
-            
-            }
-        }
+		if (Physics.Raycast(ray, out hit))
+		{
+			if (hit.collider.name == "StrikeZone")   //if the object we hit is the strikeZone Object,
+			{
+				if (this.transform.localScale.x == 0)
+				{
+					this.transform.localScale = new Vector3(STARTING_CURSOR_SIZE,STARTING_CURSOR_SIZE,STARTING_CURSOR_SIZE);
+				}
+				this.transform.position = hit.point;
+				this.transform.rotation = Quaternion.Euler(-90, 180, 180);
+				CustomMessages2.Instance.SendTargetData(1.0f,this.transform.position, this.transform.localScale);
+			
+			}
+		}
 
-        
-    }
+		
+	}
 
-    private void ScaleCursor()
-    {
-        float MIN_CURSOR_SIZE = 1.0f; 
-        float MAX_CURSOR_SIZE = 8.0f;
-        float cursorSize = this.transform.localScale.x;
+	private void ScaleCursor()
+	{
+		float MIN_CURSOR_SIZE = 1.0f; 
+		float MAX_CURSOR_SIZE = 8.0f;
+		float cursorSize = this.transform.localScale.x;
 
-        float cursorChange = Input.GetAxis("Mouse ScrollWheel") * SCALE_SPEED;
+		float cursorChange = Input.GetAxis("Mouse ScrollWheel") * SCALE_SPEED;
 
 
-        cursorSize += cursorChange;
-        cursorSize = Mathf.Min(MAX_CURSOR_SIZE, cursorSize);
-        cursorSize = Mathf.Max(MIN_CURSOR_SIZE, cursorSize);
+		cursorSize += cursorChange;
+		cursorSize = Mathf.Min(MAX_CURSOR_SIZE, cursorSize);
+		cursorSize = Mathf.Max(MIN_CURSOR_SIZE, cursorSize);
 
-        this.transform.localScale = new Vector3(cursorSize,cursorSize,cursorSize);
-        CustomMessages2.Instance.SendTargetData(1.0f,this.transform.position, this.transform.localScale);
-    }
+		this.transform.localScale = new Vector3(cursorSize,cursorSize,cursorSize);
+		CustomMessages2.Instance.SendTargetData(1.0f,this.transform.position, this.transform.localScale);
+	}
 
 }
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour {
+
+	public Slider OpacitySlider;
+    public Slider RedSlider;
+    public Slider GreenSlider;
+    public Slider BlueSlider;
+    public GameObject StrikeZoneObject;   //in the unity editor, link the strikezone gameObject to this script
+    private Renderer StrikeZoneRend;    //mesh renderer for the strike zone
+    private float opacity;
+    private float redValue;
+    private float blueValue;
+    private float greenValue;
+
+	// Use this for initialization
+	void Start () {
+        StrikeZoneRend = StrikeZoneObject.GetComponent<Renderer>();
+        Debug.Log(StrikeZoneRend);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (opacity != OpacitySlider.value)   //if the value in the slider has changed, call updateOpacity method.
+        {
+            opacity = OpacitySlider.value;
+            UpdateOpacity();
+        }
+        if (redValue != RedSlider.value || blueValue != BlueSlider.value || greenValue != GreenSlider.value)   //if any of the color slider values have changed, update color
+        {
+            redValue = RedSlider.value;
+            greenValue = GreenSlider.value;
+            blueValue = BlueSlider.value;
+
+
+            updateColor();
+        }
+
+        Debug.Log(redValue);
+
+        
+
+	}
+
+    void UpdateOpacity()    //update the opacity of the meshrenderer on the strikezone object.
+    {
+        Color newColor = StrikeZoneRend.sharedMaterial.color;
+        newColor.a = opacity;
+        StrikeZoneRend.sharedMaterial.color = newColor;
+    }
+
+    void updateColor()
+    {
+        Color newColor = StrikeZoneRend.sharedMaterial.color;
+        newColor.r = redValue;
+        newColor.g = greenValue;
+        newColor.b = blueValue;
+        StrikeZoneRend.sharedMaterial.color = newColor;
+
+    }
+}
